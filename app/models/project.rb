e79e12c63de1fd79778344abaf016d1c19d6395c
Melih Birim <melihbirim@gmail.com>
2025-02-21 15:21:59 +0000

    lint updates

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,12 @@
 class Project < ApplicationRecord
   has_many :issues, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :description, presence: true
   validates :status, presence: true
-  
+
   enum :status, [ :active, :archived ]
-  
+
   scope :active, -> { where(status: :active) }
   scope :archived, -> { where(status: :archived) }
 end
